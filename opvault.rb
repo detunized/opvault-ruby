3183b8db1f48dbffc54b0315f095b72351f822b4
Dmitry Yakimenko <detunized@gmail.com>
2018-07-26 19:08:56 +0200

    Parse folders.js

--- opvault.rb
+++ opvault.rb
@@ -4,15 +4,24 @@ require "json"
 
 def open_vault path
     profile = parse_profile File.join(path, "default/profile.js")
+    folders = parse_folders File.join(path, "default/folders.js")
 end
 
 def parse_profile filename
+    load_js_as_json filename, "var profile=", ";"
+end
+
+def parse_folders filename
+    load_js_as_json filename, "loadFolders(", ");"
+end
+
+def load_js_as_json filename, prefix, suffix
     content = File.read filename
 
-    fail "Unsupported format" if !content.start_with? "var profile={"
-    fail "Unsupported format" if !content.end_with? "};"
+    fail "Unsupported format: must start with #{prefix}" if !content.start_with? prefix
+    fail "Unsupported format: must end with #{suffix}" if !content.end_with? suffix
 
-    JSON.load content[12..-2]
+    JSON.load content[prefix.size...-suffix.size]
 end
 
 open_vault "#{ENV["HOME"]}/Downloads/opvault.opvault"
