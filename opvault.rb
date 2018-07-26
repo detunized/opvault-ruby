0d89016d427d60baa809446ee899497155ff59e6
Dmitry Yakimenko <detunized@gmail.com>
2018-07-26 20:07:17 +0200

    Derive the encryption key and MAC

--- opvault.rb
+++ opvault.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 
+require "base64"
 require "json"
+require "openssl"
 
-def open_vault path
+def open_vault path, password
     profile = load_profile path
     folders = load_folders path
     items = load_items path
 
-    ap items.keys
+    key, mac = derive_key_mac profile, password
 end
 
 def make_filename path, filename
@@ -45,14 +47,30 @@ def load_js_as_json filename, prefix, suffix
     content = File.read filename
 
     if !content.start_with? prefix
-        fail "Unsupported format: must start with #{prefix}"
+        raise "Unsupported format: must start with #{prefix}"
     end
 
     if !content.end_with? suffix
-        fail "Unsupported format: must end with #{suffix}"
+        raise "Unsupported format: must end with #{suffix}"
     end
 
     JSON.load content[prefix.size...-suffix.size]
 end
 
-open_vault "#{ENV["HOME"]}/Downloads/opvault.opvault"
+def decode64 base64
+    Base64.decode64 base64
+end
+
+def derive_key_mac profile, password
+    salt = decode64 profile["salt"]
+    iterations = profile["iterations"]
+    key_mac = pbkdf2_sha512 password, salt, iterations, 64
+
+    [key_mac[0, 32], key_mac[32, 32]]
+end
+
+def pbkdf2_sha512 password, salt, iterations, size
+    OpenSSL::PKCS5.pbkdf2_hmac password, salt, iterations, size, "sha512"
+end
+
+open_vault "#{ENV["HOME"]}/Downloads/opvault.opvault", "password"
